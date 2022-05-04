0707381e6057b6716de606bd6c0dcf8bf3ecf605
Jonas Markus Schießl <jonas.schiessl@uni-bayreuth.de>
2022-05-04 15:42:04 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,40 +1,89 @@
-# Configuration file for the Sphinx documentation builder.
-
-# -- Project information
-
-import os
+mport pdb
 import sys
+import os
 sys.path.insert(0, os.path.abspath('../../'))
 sys.path.insert(0, os.path.abspath('./'))
+
+# -- Project information -----------------------------------------------------
 
 project = 'nMPyC'
 copyright = '2022, Jonas Schießl'
 author = 'Jonas Schießl'
 
-release = '1.0'
-version = '1.0.0'
+# The full version, including alpha/beta/rc tags
+release = '1.0.0'
 
-# -- General configuration
 
-extensions = [
-    'sphinx.ext.duration',
-    'sphinx.ext.doctest',
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.intersphinx',
-]
+# -- General configuration ---------------------------------------------------
 
-intersphinx_mapping = {
-    'python': ('https://docs.python.org/3/', None),
-    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+# The master toctree document.
+master_doc = 'index'
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = ['sphinx.ext.autodoc',
+              'sphinx_rtd_theme',
+	      'sphinx.ext.intersphinx',
+          'nbsphinx',
+          'sphinx.ext.mathjax',
+          'sphinx.ext.graphviz',
+          'sphinx.ext.autosummary',
+          'sphinx.ext.viewcode',
+          'sphinx_copybutton',
+          'recommonmark'
+              ]
+
+graphviz_output_format = 'svg'
+
+autosummary_generate = True
+
+mathjax3_config = {
+    'extensions': ['tex2jax.js'],
+    'jax': ['input/TeX', 'output/HTML-CSS'],
 }
-intersphinx_disabled_domains = ['std']
 
+# Order methods in documentation by their appearence in the sourcecode:
+autodoc_member_order = 'bysource'
+# Delete this previous line, to order by alphabet.
+
+# Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
-# -- Options for HTML output
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ['_build', '**.ipynb_checkpoints']
 
+
+nbsphinx_allow_errors = True
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
 html_theme = 'sphinx_rtd_theme'
 
-# -- Options for EPUB output
-epub_show_urls = 'footnote'
+html_theme_options = {
+    'logo_only': True,
+    'navigation_depth': 4,
+}
+html_theme_path = ["../.."]
+html_logo = "static/dompc_var_02_white.svg"
+html_show_sourcelink = True
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['static']
+
+
+# -- Options for LaTeX output ---------------------------------------------
+latex_engine ='pdflatex'
+
+
+# -- Run custom script -----------------------------------------------------
+import release_overview
+
+# Get Markdown page with the overview over all releases from Github API.
+release_overview.get_overview()
