b415f1929078e4ae2cf2cad1ea796cf4f9b3d3a0
Jonas Markus Schießl <jonas.schiessl@uni-bayreuth.de>
2022-05-04 15:05:12 +0200

    edited config.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,6 +2,11 @@
 
 # -- Project information
 
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../../'))
+sys.path.insert(0, os.path.abspath('./'))
+
 project = 'nMPyC'
 copyright = '2022, Jonas Schießl'
 author = 'Jonas Schießl'
